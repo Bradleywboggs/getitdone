254660ebb5487a464ff88b4cd4ca45561039ec3e
Bradley Boggs <bradleywboggs@gmail.com>
2018-10-20 15:27:27 -0500

    Added login and register templates and handlers.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://getitdone:getitdone@localhost:8889/getitdone'
@@ -17,8 +18,115 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120))
 
-@app.route('/', methods = ['POST', 'GET'])
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+@app.route("/")
+@app.route("/login")
+def get_login():
+    return render_template("/login")
+
+@app.route("/" methods=['POST'])
+@app.route("/login" methods=['POST'])
+def post_login()
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/", methods=['POST'])
+def validate_signup():
+    username = request.form["username"]
+    password = request.form["pw"]
+    verifypw = request.form["verifypw"]
+    email = request.form["email"]
+
+    username_error = ''
+    password_error = ''
+    email_error = ''
+    verifypw_error = ''
+
+#validate username
+    if username == '':
+        username_error = "This field is required."
+        username = ''
+    elif len(username) < 3:
+        username_error = "Username is not valid."
+    elif len(username) > 20:
+        username_error = "Username is not valid."
+        username = ''
+    elif username[0] == " ":
+        username_error = "Username is not valid."
+        username = ''
+    else:
+        username = username
+        username_error = ''
+    
+# validate password
+    if password == '':
+        password_error = "This field is required."
+        password = ''
+        verifypw = ''      
+    elif len(password) < 3:
+        password_error = "Password is not valid."
+        password = ''
+        verifypw = ''
+    elif len(password) > 20:
+        password_error = "Password is not valid."
+        password = ''
+        verifypw = ''     
+# verify both passwords match
+    elif password != verifypw:
+        verifypw_error = "Passwords do not match."
+        password = ''
+        verifypw = ''
+    else:
+        username = username
+        password = password
+        verifypw = verifypw
+ 
+        
+    
+# validate email
+    if email == '':
+        email_error = ''
+        email = email
+    elif " " in email:
+        email_error = "Email is not valid."
+        email = ''
+    elif "." not in email:
+        email_error = "Email is not valid."
+        email = ''
+    elif "@" not in email:
+        email_error = "Email is not valid."
+        email = ''
+    elif len(email) < 3:
+        email_error = "Email is not valid."
+        email = ''
+    elif len(email) > 24:
+        email_error = "Email is not valid."
+        email = ''
+    else:
+        email_error = ''
+        email = email
+
+    if not username_error and not password_error and not email_error and not verifypw_error:
+        return redirect(f"/todos")
+    #render the page again with error descriptions
+    else:
+        return render_template("signup_form.html", username_error=username_error, 
+        password_error=password_error, email_error=email_error, verifypw_error=verifypw_error, username=username,
+        password='', verifypw='', email=email)
+@app.route('/todos', methods = ['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
@@ -28,7 +136,7 @@ def index():
         db.session.commit()
     
     tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    completed_tasks = Task.query.order_by(desc(pub_date)).all()
 
     return render_template('todos.html', title="Get It Done!", tasks=tasks,
         completed_tasks=completed_tasks)
