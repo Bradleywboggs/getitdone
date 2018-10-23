d14179d043700bb7fd95a182d3e76efa3475e249
Bradley Boggs <bradleywboggs@gmail.com>
2018-10-23 16:57:11 -0500

    added login and register routes and route functions for validation and db incorporation. I also added a logout route. Added a duplicate user error to the todos.html template

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -27,6 +27,90 @@ class User(db.Model):
         self.password = password
 
 
+@app.route('/login')
+def get_login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def post_login():
+    email = request.form.get('email')
+    password = request.form.get('pw')
+    user = User.query.filter_by(email=email).first()
+
+    if user and user.password == password:
+        #TODO: SESSION: 'remember' user is logged in
+        return redirect('/')
+    else:
+        return redirect('/login')
+        #TODO: FLASH: error message
+
+@app.route('/register')
+def get_register():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def post_register():
+    email = request.form['email']
+    password = request.form['pw']
+    verifypw = request.form['verifypw']
+    existing_user = User.query.filter_by(email=email).first()
+
+    # FLASH: Change errors to flashed messages
+    email_error = ''
+    password_error = ''
+    verifypw_error = ''
+    duplicate_user_error = ''
+    # Regex pattern looks for alphanumeric characters - minimum 3, no max; '@' symbol, 
+    # at least one alphanumeric, ''.'', at least 2 alphas(case insensitive)
+    email_pattern = re.compile(r'[\w]{3,}[@][\w]+[.][a-zA-Z]{2,}')
+    email_matched = email_pattern.match(email)
+    # Regex pattern checks for non-white space characters - minimum 8, maximum 20
+    pw_pattern = re.compile(r'[^\s]{8,20}')
+    pw_matched = pw_pattern.match(password)
+    
+    #verify email
+    if not email_matched:
+        email_error = 'Email not valid'
+        email = ''
+    else:
+        email_error = ''
+        email = email
+    
+    # validate password
+    if password == '':
+        password_error = 'This field is required.'
+        password = ''
+        verifypw = ''      
+    elif not pw_matched:
+        password_error = 'This password is not valid'
+        password = ''
+        verifypw = ''    
+# verify both passwords match
+    elif password != verifypw:
+        verifypw_error = 'Passwords do not match.'
+        password = ''
+        verifypw = ''
+    else:
+        password = password
+        verifypw = verifypw   
+    
+    if existing_user:
+        duplicate_user_error = 'User already exists.'
+
+    if not email_error and not password_error and not verifypw_error and not duplicate_user_error:
+        new_user = User(email, password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('register.html', 
+        password_error=password_error, email_error=email_error, verifypw_error=verifypw_error,duplicate_user_error=duplicate_user_error,
+        password='', verifypw='', email=email)
+
+@app.route('/logout')
+def log_out():
+    return redirect('/login') # TODO: SESSION 'forget' user has logged in
+
 @app.route('/', methods = ['POST', 'GET'])
 def index():
 
@@ -39,7 +123,7 @@ def index():
     tasks = Task.query.filter_by(completed=False).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
 
-    return render_template('todos.html', title="Get It Done!", tasks=tasks,
+    return render_template('todos.html', title='Get It Done!', tasks=tasks,
         completed_tasks=completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
